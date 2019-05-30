b41b33af899399e504ba311485fc55c314daa0cf
David Hacker <dmhacker@protonmail.com>
2019-05-29 18:19:04 -0700

    Add heroicons, icons to internship card

--- src/pages/company/internship_card/InternshipCard.js
+++ src/pages/company/internship_card/InternshipCard.js
@@ -4,18 +4,27 @@ import "./InternshipCard.css";
 import PropTypes from 'prop-types';
 import ExternalLinkWrapper from "../../../utils/button/ExternalLinkWrapper";
 
+import {ReactComponent as EditIcon} from "../../../img/heroicons/icon-edit.svg"
+import {ReactComponent as CheckIcon} from "../../../img/heroicons/icon-check-circle.svg"
+import {ReactComponent as FlagIcon} from "../../../img/heroicons/icon-flag.svg"
+
 export class InternshipCard extends React.PureComponent {
 
     render() {
         if (this.props.active) {
             return (
-                <ExternalLinkWrapper to={this.props.link}>
-                    <div className="is-wrapper">
-                        <div className="is-name">
-                            {this.props.name}
-                        </div>
+                <div className="is-wrapper">
+                  <ExternalLinkWrapper to={this.props.link}>
+                    <div className="is-name">
+                        {this.props.name}
                     </div>
-                </ExternalLinkWrapper>
+                  </ExternalLinkWrapper>
+                  <div className="is-buttons">
+                      <button className="is-button"><EditIcon/></button>
+                      <button className="is-button"><CheckIcon/></button>
+                      <button className="is-button"><FlagIcon/></button>
+                  </div>
+                </div>
             )
         } else {
             return (
